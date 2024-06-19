9e2b28bf3ab9836296343b7967d1e99adde8584c
manana <manana@A13E24>
2024-06-19 11:15:13 +0200

    19/06/2024

---
+++ 02_saludo_servlet/src/main/java/servlets/SaludoServlet.java
@@ -0,0 +1,33 @@
+package servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/SaludosServlet")
+public class SaludoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//lo primero es llamar al metodo setContentType
+		
+		response.setContentType("text/html");
+		
+		//se programa el printwriter con un try con recursos para que se cierre solo
+		 try(PrintWriter out=response.getWriter()){
+			 out.println("<<html><head></head><body><body><center> ");
+			 for(int i=1;i<=6;i++) {
+				 out.println("<h" +i+">Biienvenidos humanoides</h"+i+">");
+				 
+			 }
+			 out.println("</center></body></html>");// intrucción de cerrar
+		 }
+		
+	}
+
+}
---
+++ 03_tabla_multiplicar/src/main/java/servlets/TablaDeMultiplicatr.java
@@ -0,0 +1,34 @@
+package servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/TablaDeMultiplicatr")
+public class TablaDeMultiplicatr extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		
+		 try(PrintWriter out=response.getWriter()){
+			 out.println("<<html><head></head><body><body><center> ");
+			 out.println("<table border='1'>");  //para crear la primera columna 
+				for(int i=1;i<=10;i++) {
+					out.println("<tr>");  // se abre la etiqueta tr y hay que no olvidarse dar la misma instrucción para cerrarla------->out.println("</tr>");
+					for(int k=1;k<=10;k++) {
+						out.println("<td>"+i*k+"</td>");
+					}
+					out.println("</tr>");
+				}
+				out.println("</table></center></body></html>");
+			}
+		}
+
+	}
+
