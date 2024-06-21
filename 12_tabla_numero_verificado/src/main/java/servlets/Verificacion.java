8ece0af3202b4eb3803762167c883cc2ce093c80
ANA <Usuario@DESKTOP-UTV8S04>
2024-06-21 08:10:36 +0200

    12

---
+++ 12_tabla_numero_verificado/src/main/java/servlets/Verificacion.java
@@ -0,0 +1,30 @@
+package servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/verificacion")
+public class Verificacion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			Integer.parseInt(request.getParameter("numero"));
+			request.getRequestDispatcher("tabla.jsp")
+			.forward(request, response);
+			
+		
+			
+		}catch(NumberFormatException ex) {
+			
+			request.getRequestDispatcher("error.jsp")
+			.forward(request, response);
+		}
+	}
+
+}
