c5d050eb063f3f189a33f2be1b4bce52d47e92fe
manana <manana@A13E24>
2024-06-25 13:44:29 +0200

    25/06

---
+++ 24_autenticacion_cookie/src/main/java/servlets/ComprobarServlet.java
@@ -0,0 +1,54 @@
+package servlets;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.apache.tomcat.util.http.parser.Cookie;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet ("/ComprobarServlet")
+public class ComprobarServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	
+		
+		protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			// Comprobar si llega la cookie "user"
+			/*String usuario=null;
+			Cookie[] cookies=request.getCookies();
+			if(cookies!=null) {
+				for(Cookie ck:cookies) {
+					if(ck.getName().equals("usuario")) {
+						usuario=ck.getValue();
+						break;
+					}
+				}
+			}*/
+			String usuario=Arrays.stream(request.getCookies()!=null?request.getCookies():new Cookie[0])
+						.filter(c->c.getName().equals("usuario"))
+						.findFirst()
+						.orElse(new Cookie("usuario",null))
+						.getValue();
+			
+			if(usuario==null) {
+				request.getRequestDispatcher("login.html").forward(request, response);
+			}else {
+				//recoge la ficha del usuario antes de pasar el control a bienvenida.jsp
+				UsuariosService service=new UsuariosService();
+				request.setAttribute("ficha", service.fichaUsuario(usuario));
+				request.getRequestDispatcher("bienvenida.jsp").forward(request, response);
+			}
+		}
+
+	
+		
+		
+	}
+
+
---
+++ 24_autenticacion_cookie/src/main/java/servlets/Login.java
@@ -0,0 +1,44 @@
+package servlets;
+
+import java.io.IOException;
+
+import org.apache.tomcat.util.http.parser.Cookie;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.Ficha;
+import service.AutenticacionService;
+
+@WebServlet("/Login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException  {
+	String user=request.getParameter("username");
+	String pwd=request.getParameter("password");
+	RequestDispatcher dispatcher;
+	//instancio la clase service
+	var service = new AutenticacionService();
+	
+	if(service.autenticar(user, pwd)) {
+	//creamos la ficha 	del usuario y la guardamos en un atributo
+	Ficha ficha = service.fichausuario(user);// creoi el objeto
+	request.setAttribute("ficha", ficha);//lo guardo
+		dispatcher=request.getRequestDispatcher("bienvenida.jsp");//envio la peticion
+	}else {
+		dispatcher=request.getRequestDispatcher("error.jsp");
+	}
+	dispatcher.forward(request, response);
+}
+	private void crearCookie(HttpServletResponse response) {
+	Cookie cookie = new Cookie("usuario",usuario);
+
+		Cookie ck=new Cookie( user);
+		
+		response.addCookie(ck);
+
+}
